233980d15add2852ec078015b0bcd1ff398a69be
Marcel Pluntke <111451037+Marcel-Pluntke@users.noreply.github.com>
2023-05-30 22:57:52 +0200

    Update Python.py

--- Python.py
+++ Python.py
@@ -103,4 +103,27 @@ print(bool(()))
 print(bool(''))
 print(bool(range(0)))
 print(bool(set()))
-# See Logical Operators and Comparison Operators section for more on booleans.
+
+# lists
+my_list = [1, 2, '3', True]# We assume this list won't mutate for each example 
+below
+len(my_list) # 4
+my_list.index('3') # 2
+my_list.count(2) # 1 --> count how many times 2 appears
+my_list[3] # True
+my_list[1:] # [2, '3', True]
+my_list[:1] # [1]
+my_list[-1] # True
+my_list[::1] # [1, 2, '3', True]
+my_list[::-1] # [True, '3', 2, 1]
+my_list[0:3:2] # [1, '3']
+# : is called slicing and has the format [ start : end : step ]
+
+# Add to List
+my_list * 2 # [1, 2, '3', True, 1, 2, '3', True]
+my_list + [100] # [1, 2, '3', True, 100] --> doesn't mutate original list, creates new one
+my_list.append(100) # None --> Mutates original list to [1, 2, '3', True, 100] # Or: <list> += [<el>]
+my_list.extend([100, 200]) # None --> Mutates original list to [1, 2, '3', True, 100, 200]
+my_list.insert(2, '!!!') # None --> [1, 2, '!!!', '3', True] - Inserts item at index and moves the rest to the right.
+' '.join(['Hello','There'])# 'Hello There' --> Joins elements using string as separator.
+
